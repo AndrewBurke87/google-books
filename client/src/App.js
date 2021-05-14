66099bb374187a81a4f1928ba23ecd0cb7db7803
Andrew Burke <Drewburke39@gmail.com>
2021-05-14 17:40:25 -0600

    started over with new components using the code provided from week 21 activity 5

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,24 @@ import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Nav />
-                <Switch>
-                    <Route exact path={["/", "/books"]}>
-                        <Books />
-                    </Route>
-                    <Route exact path="/books/:id">
-                        <Detail />
-                    </Route>
-                    <Route>
-                        <NoMatch />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
